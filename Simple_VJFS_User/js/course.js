66638930520343ea22247802c9181dac900b647d
vivekk <vivekk@ifi.uio.no>
2014-11-20 17:07:28 +0100

    course.js > The course description can now contain links without it destroying the whole design

--- Simple_VJFS_User/js/course.js
+++ Simple_VJFS_User/js/course.js
@@ -14,20 +14,27 @@ function displayCourses() {
 			// Display courses
 			for(key in courses['courses']) {
 
+				var desc = JSON.parse(courses['courses'][key].courseDescription);
 
-				var desc = courses['courses'][key].courseDescription;
-				desc = desc.substr(0, desc.length-1);                   //fjerner siste char i stringen
-				desc = desc.substr(1);       //fjerner første char i stringen
+				var l = '<div class="panel panel-default">'
+				l +=  '<div class="panel-heading"><h4>'+ courses['courses'][key].courseTitle +'</h4></div>'
+				l +=  '<div class="panel-body" >'+desc+'</div>'
+				l += '<div class="panel-footer" id='+courses['courses'][key].courseID+'></div>'
+				l += '</div>'
+
+
+				/*
+				var desc = JSON.parse(courses['courses'][key].courseDescription);
 
 	            var l = '<div class="list-group" id='+courses['courses'][key].courseID+'>';
 
 				l+= '<a href="#" class="list-group-item disabled">';
     			l+=	'<h4 class="list-group-item-heading">'+ courses['courses'][key].courseTitle +'</h4>';
-    			l+=	'<p class="list-group-item-text">'+desc+'</p>';
+    			l+=	'<h4>'+desc+'</h4>';
  				l+= '</a>'
 
  				l+='</div>';
-
+				*/
 				$('#courses').append(l);
 
 			}
@@ -42,13 +49,14 @@ function displayQuizes(course){
 	getUserQuizes(function(userQuizes) {
 		if(userQuizes != null) {
 
-
-			for(quiz_key in userQuizes) {
-				console.log(userQuizes[quiz_key].quizID)
+			var course_quizes = $.grep(userQuizes['quizes'], function(e){ return e.courseID == course['courseID']; });
 
 
+			for(quiz_key in course_quizes) {
+				 console.log(course_quizes[quiz_key].quizID)
+				console.log("asdfasdfa");
 			}
-
+		
 		}
 
 	});
@@ -59,8 +67,6 @@ function displayQuizes(course){
 
 			var course_quizes = $.grep(quizes['quizes'], function(e){ return e.courseID == course['courseID']; });
 
-
-
 			course_quizes.sort(function(a, b) {
 				if(a.quizLevel < b.quizLevel) return -1;
 				if(a.quizLevel > b.quizLevel) return 1;
@@ -68,9 +74,14 @@ function displayQuizes(course){
 			});
 
 			for(quiz_key in course_quizes) {
-				
-				var l='<a href="pages/questions.html?quiz_id='+ course_quizes[quiz_key].quizID + '&course_id=' + course['courseID']+'" class="list-group-item">'+ course_quizes[quiz_key].quizLevel+' - '+course_quizes[quiz_key].quizTitle+'  </a>';
-				$('#'+course['courseID']).append(l);
+				 
+				var t='<a href="pages/questions.html?quiz_id='+ course_quizes[quiz_key].quizID + '&course_id=';
+				t += course['courseID']+'" class="list-group-item">';
+				t += course_quizes[quiz_key].quizLevel+' - '+course_quizes[quiz_key].quizTitle;
+				t += '<div id='+course_quizes[quiz_key].quizID+'></div>'
+				t +='</a>';
+
+				$('#'+course['courseID']).append(t);
 
 					
 			}
