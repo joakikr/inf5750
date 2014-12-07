f9c998a7d7fdaf457ce71730de4d5afba24b1897
babajon <farrukhm@ifi.uio.no>
2014-12-07 21:44:40 +0100

    Added link visibility restrictions
    Added link visibility restrictions

--- VJFS_User/js/main.js
+++ VJFS_User/js/main.js
@@ -36,3 +36,125 @@ function getAppRoot() {
 	return getHostRoot() + '/apps/VJFS_User';
 }
 
+/**
+ * Function will check if the user currently trying to access app
+ * has the right privilege level (admin or if groups can be created: customizer)
+ *
+ * If user is admin, then handler function is called
+ */
+function isCustomizer(handler) {
+	// Create URL to fetch user data from (was not possible to use "fields" and "filters" here?)
+	// If so then last part of URL would be: /api/me.json?fields=userCredentials[userAuthorityGroups]
+	var url = getHostRoot() + '/api/me';
+
+	// Get information as json object
+	$.ajax({
+		url: url,
+		dataType: 'json'
+	}).success(function(user) {
+
+		var isSuperUser = false;
+
+		if(user.hasOwnProperty('userCredentials')) {
+			// Someone logged in,
+			// Need to check if user is "Superuser"
+			var authority_groups = user['userCredentials']['userAuthorityGroups'];
+
+			for(key in authority_groups) {
+				if(authority_groups[key]['name'] === 'Superuser') {
+					// This is a super user!
+					isSuperUser = true;
+					break;
+				}
+			}
+		}
+		if(isSuperUser) {
+			// Here we have a super user, call handler function
+			handler(true);
+		} else {
+			handler(false);
+		}
+	});
+}
+function navBarElements(){
+	isCourseAttendant(function(isCourseAttendant) {
+		if(!isCourseAttendant){
+			document.getElementById("showcourse").style.display = "none";
+			console.log();
+		}
+	});
+	isCourseMentor(function(isCourseMentor) {
+		if(!isCourseMentor){
+			document.getElementById("showmentor").style.display = "none";
+		}
+	});
+	isCustomizer(function(isCustomizer) {
+		if(!isCustomizer) {
+			document.getElementById("showadmin").style.display = "none";
+			document.getElementById("showstat").style.display = "none";
+		}
+	});
+}
+
+function isCourseAttendant(handler){
+	var meurl = getHostRoot() + '/api/me';
+	var courseurl = getHostRoot() + '/api/systemSettings/VJFS_courses';
+	$.ajax({
+		url: meurl,
+		dataType: 'json'
+	}).success(function(userinfo) {
+		$.ajax({
+			url: courseurl,
+			dataType: 'json'
+		}).success(function(courses) {
+			var isAttendant = false;
+			var userid = userinfo['id'];
+			//console.log(userid);
+			for(key in courses['courses']){
+				for(keys in courses['courses'][key]['courseAttendants']){
+					if(courses['courses'][key]['courseAttendants'][keys].attendantID === userid){
+						isAttendant = true;
+						break;
+					}
+				}
+			}
+			if(isAttendant){
+				handler(true);
+			} else {
+				handler(false);
+			}
+		});
+
+	});
+}
+function isCourseMentor(handler){
+	var meurl = getHostRoot() + '/api/me';
+	var courseurl = getHostRoot() + '/api/systemSettings/VJFS_courses';
+	$.ajax({
+		url: meurl,
+		dataType: 'json'
+	}).success(function(userinfo) {
+		$.ajax({
+			url: courseurl,
+			dataType: 'json'
+		}).success(function(courses) {
+			var isMentor = false;
+			var userid = userinfo['id'];
+			//console.log(userid);
+			for(key in courses['courses']){
+				for(keys in courses['courses'][key]['courseMentors']){
+					if(courses['courses'][key]['courseMentors'][keys].mentorID === userid){
+						isMentor = true;
+						break;
+					}
+				}
+			}
+			if(isMentor){
+				handler(true);
+			} else {
+				handler(false);
+			}
+		});
+
+	});
+}
